91d5af71181def99e8a9857a5db3f0bd4cfdcff9
dinesh-ng <adultimprover@gmail.com>
2023-02-15 23:48:37 +0530

    use map() for leaf data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Leaf from "./components/Leaf";
 import leafData from "./leafData";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Leaf {...leafData[0]} />
-      <Leaf {...leafData[1]} />
-      <Leaf {...leafData[2]} />
-      {/* {leafData.map((leaf) => (
-        <Leaf title={leaf.title} content={leaf.content} />
-      ))} */}
+      {leafData.map((leaf) => (
+        <Leaf key={leaf.id} title={leaf.title} content={leaf.content} />
+      ))}
       <Footer />
     </div>
   );
--- src/leafData.js
+++ src/leafData.js
@@ -1,13 +1,16 @@
 const leafData = [
   {
+    id: 1,
     title: "Arrays and Strings",
     content: "Complete last two.",
   },
   {
+    id: 2,
     title: "Strings",
     content: "Have to start.",
   },
   {
+    id: 3,
     title: "Dev",
     content: "Dice project: disconnect issue.",
   },
